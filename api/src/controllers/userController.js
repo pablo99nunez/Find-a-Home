184ce60b5d9b20deb007dd723a5e33dfc9acff61
flopitt90 <flopitta_90@hotmail.com>
2023-01-17 17:13:26 +0000

    routes user and constroller fixed

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,44 +1,27 @@
 const UserModel = require('../models/user');
 
-const createNewUser = async (userData) => {
-try {
-  const newUser = await UserModel.create(userData, function(err,user){
-    if(err){throw new Error('We could not add the user to our data base')}
-    else return newUser
-  })
-} catch (error) {
-  return error
-}
+const createNewUser = async (user) => {
+  const newUser = new UserModel(user)
+  await newUser.save()
+  return newUser
 }
 
 const findUser = async (userData) => {
-  try {
     const userInDB = await UserModel.findOne(userData)
-    if (userInDB) {return userInDB}
-    else return false
-  } catch (error) {
-    return error
-  }
+    return userInDB
+ 
 }
 
-const findAllUsers = async (filter = {}) => {
-  try {
-    const allUsers = await UserModel.find(filter)
-    if (allUsers) allUsers;
-    else return false
-  } catch (error) {
-    return error
-  }
+const findAllUsers = async (filter) => {
+    const allUsers = await UserModel.find()
+    return allUsers
+ 
 }
 
 const updateUser = async(userData) => {
-  try {
    const user = {email: userData.email}
    const updatedUser = await UserModel.updateOne(user, userData)
    return updatedUser
-  } catch (error) {
-    return error
-  }
 }
 
 module.exports = {
--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -3,41 +3,43 @@ const { findUser, updateUser, findAllUsers, createNewUser } = require('../contro
 const router = express.Router();
 const UserModel = require('../models/user');
 
-router.get('/profile',(req, res) => {
+router.get('/profile',async (req, res) => {
   try {
     const userEmail = req.body.email
     if(userEmail) {
-      const user = findUser(userEmail)}
+      const user = await findUser(userEmail)}
       res.send(user)
   } catch (error) {
     res.status(501).send({error: error.message})
   }
 })
 
-router.get('/', (req,res) => {
+router.get('/', async(req,res) => {
   try {
     const user = req.query
-    if(!user) user = {}
-    findAllUsers(user)
+    if(!user) user = ''
+    const users = await findAllUsers(user)
+    res.status(200).send(users)
   } catch (error) {
     res.status(501).send({error: error.message})
   }
 })
 
-router.post('/', (req,res) => {
+router.post('/',async (req,res) => {
   try {
-    const newUser = req.body
-    createNewUser(newUser)
+    const user = req.body
+    const createdUser = await createNewUser(user)
+    res.status(200).send(createdUser)
   } catch (error) {
     res.status(501).send({error: error.message})
   }
 })
 
-router.put('/',(req, res) => {
+router.put('/', async(req, res) => {
   try{
     const user = req.body
-    const updatedUser = updateUser(user)
-  res.send({message: 'usuario editado', payload: updatedUser });
+    const updatedUser = await updateUser(user)
+  res.status(200).send({message: 'usuario editado', payload: updatedUser });
 } catch(err){
   res.status(501).send({error: err.message})
 }
