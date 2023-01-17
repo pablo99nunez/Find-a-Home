001fa8a48b95fe80dcab3d86b9a57b13dc2ebc94
GabrielAndresCruz <andrescruz048@hotmail.com>
2023-01-17 17:04:23 -0300

    Third try for App solution

--- client/App.js
+++ client/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Find a Home, Pets App!</Text>
+      <Text>Find a Home</Text>
       <StatusBar style="auto" />
     </View>
   );
