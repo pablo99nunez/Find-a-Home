cbf085791e0e65ba8bab7a435dd417d76933b6da
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-01-18 15:17:40 -0300

    styles test
    cambié el margen horizontal para comprobar algo

--- client/App.js
+++ client/App.js
@@ -1,11 +1,11 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./src/Components/Home/Home";
-import LandingPage from './src/Components/LandingPage/LandingPage';
-import Welcome from './src/Components/WelcomePage/WelcomePage';
+import LandingPage from "./src/Components/LandingPage/LandingPage";
+import Welcome from "./src/Components/WelcomePage/WelcomePage";
 import Detail from "./src/Components/Detail/Detail";
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
@@ -14,22 +14,12 @@ export default function App() {
         <Stack.Screen
           name="LandigPage"
           component={LandingPage}
-          initialParams={{ fromChild: 'Initial' }}
+          initialParams={{ fromChild: "Initial" }}
         />
-        <Stack.Screen 
-          name="Welcome" 
-          component={Welcome} 
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-        />
-        <Stack.Screen
-          name="Detail" 
-          component={Detail}
-        />
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
       </Stack.Navigator>
     </NavigationContainer>
-    
   );
 }
