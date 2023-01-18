b6b8b6e4824a395e16c868a7c302aaad21fc4168
flopitt90 <flopitta_90@hotmail.com>
2023-01-18 10:10:35 +0000

    routes user fixed

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -26,8 +26,8 @@ router.get('/', async(req,res) => {
 
 router.post('/',async (req,res) => {
   try {
-    const {email} = req.body
-    const createdUser = await createNewUser(email)
+    const user = req.body
+    const createdUser = await createNewUser(user)
     res.status(200).send(createdUser)
   } catch (error) {
     res.status(501).send({error: error.message})
