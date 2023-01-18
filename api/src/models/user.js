dbc9c374069773be710e992228c8e21049de7915
ebootdpr <rod.toobe@gmail.com>
2023-01-18 20:13:31 -0300

    fix/changed models/feat/added db validations

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -92,16 +92,9 @@ const userSchema = mongoose.Schema(
   }
 );
 
-// add plugin that converts mongoose to json
 userSchema.plugin(toJSON);
 //userSchema.plugin(paginate);
 
-/**
- * Check if email is taken
- * @param {string} email - The user's email
- * @param {ObjectId} [excludeUserId] - The id of the user to be excluded
- * @returns {Promise<boolean>}
- */
 userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
   const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
   return !!user;
