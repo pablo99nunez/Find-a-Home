4b6eda7bc1aef2fc7d86b6d58f7d9ec816075e00
flopitt90 <flopitta_90@hotmail.com>
2023-01-19 16:30:42 +0000

    nativewind installed (tailwind)

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -25,8 +25,7 @@ export default function App({ navigation }) {
       "https://thumbs.dreamstime.com/b/gatito-gris-durmiendo-en-el-sof%C3%A1-gato-armario-de-la-casa-162888338.jpg"
   ]},
     { id: 3, name: "cheems", age: 3, rescued: "3 days ago" , profilePic:'https://www.latercera.com/resizer/gnJjKC7qAZDFZLjJDzc0kdlgXWI=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/PDW3FDGMEBGVXHM77NEEDMY7KU.jpg',createdAt: "2023-01-07T22:58:33.462Z",  gallery: [
-      "https://img.freepik.com/fotos-premium/perro-callejero-marron-que-ha-sido-cuidado-mirada-sospecha_49071-4515.jpg?w=360","https://img.freepik.com/fotos-premium/perro-callejero-marron-que-ha-sido-cuidado-mirada-sospecha_49071-4517.jpg?w=2000"
-  ]},
+      "https://img.freepik.com/fotos-premium/perro-callejero-marron-que-ha-sido-cuidado-mirada-sospecha_49071-4515.jpg?w=360","https://img.freepik.com/fotos-premium/perro-callejero-marron-que-ha-sido-cuidado-mirada-sospecha_49071-4517.jpg?w=2000"]},
     { id: 4, name: "cheems samurai", age: 4, rescued: "4 days ago", profilePic:'https://www.consumer.es/app/uploads/2019/07/img_perro-rescatado.jpg',createdAt: "2023-01-10T22:58:33.462Z"},
     { id: 5, name: "firulais", age: 5, rescued: "5 days ago", profilePic: 'https://www.hogarmania.com/archivos/202004/chequeo-veterinario-en-gatos-recogidos-de-la-calle-1280x720x80xX.jpg', createdAt: "2023-01-12T22:58:33.462Z"},
     { id: 6, name: "cachupin", age: 3, rescued: "4 days ago",profilePic: 'https://images.theconversation.com/files/465530/original/file-20220526-24-59vyo6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C5194%2C3452&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip',createdAt: "2023-01-13T22:58:33.462Z" },
