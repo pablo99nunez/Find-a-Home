a04af1ad3b2ec153332ce5e7a108ed8d489ca9ce
LegalBrian <legalbrian11@gmail.com>
2023-01-19 21:53:47 -0300

    pets added

--- client/src/Components/UserDetail/UserDetail.jsx
+++ client/src/Components/UserDetail/UserDetail.jsx
@@ -7,6 +7,7 @@ import {
   ImageBackground,
   TouchableOpacity,
   Dimensions,
+  FlatList,
 } from "react-native";
 import {LinearGradient} from "expo-linear-gradient"
 
@@ -23,7 +24,50 @@ export default function UserDetail({route, navigation}){
     rating: 5 , 
     profilePic: 'https://pbs.twimg.com/media/FdSKUwgWIAEJNX0.jpg', 
     description: "Soy Lionel Andrés Messi Cuccittini, nací en Rosario, el 24 de junio de 1987, soy conocido como Leo Messi, soy un futbolista argentino que juega como delantero o centrocampista. Jugador histórico del Fútbol Club Barcelona, al que estuve ligado veinte años, y desde 2021 integro el plantel del Paris Saint-Germain de la Ligue 1 de Francia.",
-    address: "Torre Eiffel"
+    address: "Torre Eiffel",
+    pets: [{ 
+      id: 100, 
+      name: "mbappe", 
+      age: 2, 
+      rescued: "2 days ago" , 
+      profilePic: 'https://d7lju56vlbdri.cloudfront.net/var/ezwebin_site/storage/images/_aliases/img_1col/noticias/publican-una-secuencia-genomica-de-alta-calidad-de-la-tortuga-gigante-de-seychelles/9851713-3-esl-MX/Publican-una-secuencia-genomica-de-alta-calidad-de-la-tortuga-gigante-de-Seychelles.png', 
+      createdAt: "2023-01-01T22:58:33.462Z", 
+      gallery: [
+      "https://d7lju56vlbdri.cloudfront.net/var/ezwebin_site/storage/images/_aliases/img_1col/noticias/publican-una-secuencia-genomica-de-alta-calidad-de-la-tortuga-gigante-de-seychelles/9851713-3-esl-MX/Publican-una-secuencia-genomica-de-alta-calidad-de-la-tortuga-gigante-de-Seychelles.png",
+      "https://d7lju56vlbdri.cloudfront.net/var/ezwebin_site/storage/images/_aliases/img_1col/noticias/publican-una-secuencia-genomica-de-alta-calidad-de-la-tortuga-gigante-de-seychelles/9851713-3-esl-MX/Publican-una-secuencia-genomica-de-alta-calidad-de-la-tortuga-gigante-de-Seychelles.png",
+      "https://d7lju56vlbdri.cloudfront.net/var/ezwebin_site/storage/images/_aliases/img_1col/noticias/publican-una-secuencia-genomica-de-alta-calidad-de-la-tortuga-gigante-de-seychelles/9851713-3-esl-MX/Publican-una-secuencia-genomica-de-alta-calidad-de-la-tortuga-gigante-de-Seychelles.png",
+      ], 
+      description: "Segundo"
+    },
+    { 
+      id: 200, 
+      name: "mauricio", 
+      age: 2, 
+      rescued: "2 days ago" , 
+      profilePic: 'https://www.elmueble.com/medio/2022/10/07/gato-munchkin-o-gato-salchicha_fb219737_900x900.jpg', 
+      createdAt: "2023-01-01T22:58:33.462Z", 
+      gallery: [
+      "https://www.elmueble.com/medio/2022/10/07/gato-munchkin-o-gato-salchicha_fb219737_900x900.jpg",
+      "https://www.elmueble.com/medio/2022/10/07/gato-munchkin-o-gato-salchicha_fb219737_900x900.jpg",
+      "https://www.elmueble.com/medio/2022/10/07/gato-munchkin-o-gato-salchicha_fb219737_900x900.jpg",
+      ], 
+      description: "No se inunda mas"
+    },
+    { 
+      id: 300, 
+      name: "juan", 
+      age: 2, 
+      rescued: "2 days ago" , 
+      profilePic: 'https://static.wikia.nocookie.net/mamarre-estudios-espanol/images/a/a3/FB_IMG_1596591789564.jpg/revision/latest?cb=20200806023457&path-prefix=es', 
+      createdAt: "2023-01-01T22:58:33.462Z", 
+      gallery: [
+      "https://static.wikia.nocookie.net/mamarre-estudios-espanol/images/a/a3/FB_IMG_1596591789564.jpg/revision/latest?cb=20200806023457&path-prefix=es",
+      "https://static.wikia.nocookie.net/mamarre-estudios-espanol/images/a/a3/FB_IMG_1596591789564.jpg/revision/latest?cb=20200806023457&path-prefix=es",
+      "https://static.wikia.nocookie.net/mamarre-estudios-espanol/images/a/a3/FB_IMG_1596591789564.jpg/revision/latest?cb=20200806023457&path-prefix=es",
+      ], 
+      description: "Juan 👍"
+    },
+    ]
   }
 
   return(
@@ -58,7 +102,30 @@ export default function UserDetail({route, navigation}){
           </View>
         </LinearGradient> 
       </ImageBackground>
-      <Text className="absolute bottom-32 text-xl text-center m-10">{user.description}</Text>
+      <Text className="absolute bottom-60 text-base text-center m-10">{user.description}</Text>
+      <View className="absolute bottom-20">
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={user.pets}
+          horizontal={true}
+          renderItem={({ item }) => (
+            <TouchableOpacity 
+              className="grid grid-rows-3"
+              onPress={() => navigation.navigate("Detail", item)}
+            >
+              <View className="flex items-center m-4">
+                <Text>{item.name}</Text>
+                <Image
+                  style={{ width: 100, height: 100}}
+                  source={{
+                    uri: item.profilePic,
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+          )}
+        ></FlatList>
+      </View>
     </View>
   )
 }
