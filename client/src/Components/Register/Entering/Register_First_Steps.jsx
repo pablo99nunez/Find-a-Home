767e0ac9d5d4e8ae1393c56b9b1eafec4a57a559
HiUlisesOk <uesquivel95@gmail.com>
2023-01-20 10:50:51 -0300

    register fixed

--- client/src/Components/Register/Entering/Register_First_Steps.jsx
+++ client/src/Components/Register/Entering/Register_First_Steps.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useRef } from "react";
 import { Picker } from '@react-native-picker/picker';
 import { View, Text, Dimensions, StyleSheet, ScrollView, TextInput, TouchableOpacity, Image } from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-const WIDTH = Dimensions.get("window").width;
-const HEIGHT = Dimensions.get("window").height;
 
 export default function RegisterFirstSteps({ navigation }) {
 
@@ -20,81 +19,84 @@ export default function RegisterFirstSteps({ navigation }) {
 
 
 	return (
+
 		<View className="flex flex-col" style={styles.container}>
+			<KeyboardAwareScrollView
+				style={{ flex: 1, width: '100%' }}
+				keyboardShouldPersistTaps="always">
+
+				<View className="items-end" style={styles.divisionPanel}>
+					<Image
+						style={styles.icon}
+						source={require("../../../images/logo-black.png")}
+					/>
+				</View>
 
 
-			<View className="items-end" style={styles.divisionPanel}>
-				<Image
-					style={styles.icon}
-					source={require("../../../images/logo-black.png")}
-				/>
-			</View>
+				<View className="items-end" style={styles.divisionPanel}>
+					<Text style={styles.textTitles}>¡Bienvenido!</Text>
+					<Text style={styles.textSubTitles}>Solo unos datos más y</Text>
+					<Text style={styles.textSubTitles}>podrás comenzar:</Text>
+				</View>
+
+				<View style={styles.divisionPanel}>
+
+					<Text style={styles.text}>Telefono</Text>
+					<TextInput
+						style={styles.input}
+						value={userInput}
+						placeholder={"   011 555-5555"}
+						placeholderTextColor="#ffffff50"
+						onChangeText={(text) => setUserInput({ ...userInput, telefono: text })}
+					/>
+
+				</View>
 
 
-			<View className="items-end" style={styles.divisionPanel}>
-				<Text style={styles.textTitles}>¡Bienvenido!</Text>
-				<Text style={styles.textSubTitles}>Solo unos datos más y</Text>
-				<Text style={styles.textSubTitles}>podrás comenzar:</Text>
-			</View>
-
-			<View style={styles.divisionPanel}>
-
-				<Text style={styles.text}>Telefono</Text>
-				<TextInput
-					style={styles.input}
-					value={userInput}
-					placeholder={"   011 555-5555"}
-					placeholderTextColor="#ffffff50"
-					onChangeText={(text) => setUserInput({ ...userInput, telefono: text })}
-				/>
-
-			</View>
+				<View style={styles.divisionPanel}>
+					<Text style={styles.text}>País:</Text>
+					<Picker style={styles.inputPicker}
+						ref={pickerRef}
+						selectedValue={userInput.pais}
+						onValueChange={(itemValue, itemIndex) =>
+							setUserInput({ ...userInput, pais: itemValue })
+						}>
+						<Picker.Item label="Argentina" value="Argentina" />
+						<Picker.Item label="Francia" value="Francia" />
+						<Picker.Item label="Croacia" value="Croacia" />
+					</Picker>
+				</View>
 
 
-			<View style={styles.divisionPanel}>
-				<Text style={styles.text}>País:</Text>
-				<Picker style={styles.inputPicker}
-					ref={pickerRef}
-					selectedValue={userInput.pais}
-					onValueChange={(itemValue, itemIndex) =>
-						setUserInput({ ...userInput, pais: itemValue })
-					}>
-					<Picker.Item label="Argentina" value="Argentina" />
-					<Picker.Item label="Francia" value="Francia" />
-					<Picker.Item label="Croacia" value="Croacia" />
-				</Picker>
-			</View>
+				<View style={styles.divisionPanel}>
+					<Text style={styles.text}>Provincia:</Text>
+					<Picker style={styles.inputPicker}
+						ref={pickerRef}
+						selectedValue={userInput.provincia}
+						onValueChange={(itemValue, itemIndex) =>
+							setUserInput({ ...userInput, provincia: itemValue })
+						}>
+						<Picker.Item label="La rioja" value="La rioja" />
+						<Picker.Item label="Mendoza" value="Mendoza" />
+						<Picker.Item label="Buenos Aires" value="Buenos Aires" />
+					</Picker>
+				</View>
 
 
-			<View style={styles.divisionPanel}>
-				<Text style={styles.text}>Provincia:</Text>
-				<Picker style={styles.inputPicker}
-					ref={pickerRef}
-					selectedValue={userInput.provincia}
-					onValueChange={(itemValue, itemIndex) =>
-						setUserInput({ ...userInput, provincia: itemValue })
-					}>
-					<Picker.Item label="La rioja" value="La rioja" />
-					<Picker.Item label="Mendoza" value="Mendoza" />
-					<Picker.Item label="Buenos Aires" value="Buenos Aires" />
-				</Picker>
-			</View>
-
-
-			<View style={styles.divisionPanel}>
-				<Text style={styles.text}>Departamento:</Text>
-				<Picker style={styles.inputPicker}
-					ref={pickerRef}
-					selectedValue={userInput.departamento}
-					onValueChange={(itemValue, itemIndex) =>
-						setUserInput({ ...userInput, departamento: itemValue })
-					}>
-					<Picker.Item label="depto1" value="depto1" />
-					<Picker.Item label="depto2" value="depto2" />
-					<Picker.Item label="CABA" value="CABA" />
-				</Picker>
-			</View>
-
+				<View style={styles.divisionPanel}>
+					<Text style={styles.text}>Departamento:</Text>
+					<Picker style={styles.inputPicker}
+						ref={pickerRef}
+						selectedValue={userInput.departamento}
+						onValueChange={(itemValue, itemIndex) =>
+							setUserInput({ ...userInput, departamento: itemValue })
+						}>
+						<Picker.Item label="depto1" value="depto1" />
+						<Picker.Item label="depto2" value="depto2" />
+						<Picker.Item label="CABA" value="CABA" />
+					</Picker>
+				</View>
+			</KeyboardAwareScrollView>
 			<View className="items-end mt-10" style={styles.divisionPanel}>
 
 				<TouchableOpacity
@@ -115,8 +117,8 @@ export default function RegisterFirstSteps({ navigation }) {
 const styles = StyleSheet.create({
 	container: {
 		padding: 15,
-		width: WIDTH,
-		height: HEIGHT,
+		flex: 1,
+		alignItems: 'center',
 		backgroundColor: '#FFC733',
 	},
 	icon: {
