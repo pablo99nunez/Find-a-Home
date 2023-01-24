3c1b510f78f4dbf7b96b94cffe473d90f13f1203
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-01-24 18:48:27 +0100

    Pet image complete!

--- client/src/Redux/Actions/index.js
+++ client/src/Redux/Actions/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import { BASE_URL_IP } from "@env"
-export const url = BASE_URL_IP || `http://192.168.0.14:8080/`
-if(!BASE_URL_IP){
-    alert("No se cargó bien el .env! Ejemplo: BASE_URL_IP=http://192.168.0.14:8080/")
-}
+// export const url = BASE_URL_IP || `http://192.168.178.211:8080`
+// if(!BASE_URL_IP){
+//     alert("No se cargó bien el .env! Ejemplo: BASE_URL_IP=http://192.168.0.14:8080/")
+// }
 const GET_ALL_PETS = 'GET_ALL_PETS'
 const GET_PETS_FILTERED_SPECIE = 'GET_PETS_FILTERED_SPECIE'
 const GET_PETS_FILTERED_SIZE = 'GET_PETS_FILTERED_SIZE'
@@ -49,3 +49,10 @@ export const getPetsFilteredByTwoFilters = (payload) => {
     }
 }
 
+export const PetPost = (payload) => {
+    return async function () {
+        const petPost = await axios.post(url + `/pet`, payload)
+        return petPost
+
+    }
+}
