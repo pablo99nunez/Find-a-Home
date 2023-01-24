0dee108b8f576511da6be99ba9f5f5556eccb702
GabrielAndresCruz <andrescruz048@hotmail.com>
2023-01-23 22:14:23 -0300

    Put on Work Filters

--- client/src/Redux/Reducers/index.js
+++ client/src/Redux/Reducers/index.js
@@ -1,13 +1,26 @@
 const GET_ALL_PETS = 'GET_ALL_PETS'
+const GET_PETS_FILTERED_SPECIE = 'GET_PETS_FILTERED_SPECIE'
+const GET_PETS_FILTERED_SIZE = 'GET_PETS_FILTERED_SIZE'
 
 const initialState = {
-    allPets: []
+    allPets: [],
+    filteredPets: []
 }
 // console.log(initialState.allPets);
 const rootReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case GET_ALL_PETS:
+            return {
+                ...state,
+                allPets: action.payload
+            }
+        case GET_PETS_FILTERED_SPECIE:
+            return {
+                ...state,
+                allPets: action.payload
+            }
+        case GET_PETS_FILTERED_SIZE:
             return {
                 ...state,
                 allPets: action.payload
