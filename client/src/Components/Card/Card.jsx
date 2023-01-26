4b9a07cf91fadb373a7e66ba60cbed5526a2f01e
LegalBrian <legalbrian11@gmail.com>
2023-01-26 18:19:25 -0300

    solucionado el import del rating y gradiente agregado a las cards

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
 
 const { width, height } = Dimensions.get("screen");
 
 const Card = ({item}) => {
     return (
-        <View className="flex justify-center items-center text-center">
+        <View className="flex justify-center items-center text-center" style={styles.container}>
             <Image
+                className="relative"
                 style={styles.imagen}
                 source={{uri: item.profilePic}}
             />
-            <Text className="absolute bottom-1" style={styles.dogName}>{item.name}</Text>
+            <LinearGradient
+                    className="absolute"
+                    colors={["#00000000", "rgba(0, 0, 0, 0.7)"]}
+                    style={styles.gradient}
+            >
+            </LinearGradient>
+            <Text className="absolute bottom-1 text-center items-center" style={styles.dogName}>{item.name}</Text>
         </View>
     )
 }
 const styles = StyleSheet.create({
+container: {
+    width: width * 0.4,
+    height: width * 0.4,
+    margin: 15
+},
 dogName: {
-    marginTop: 24,
-    padding: 15,
     fontSize: 25,
-    color: 'white'
-  },
-  imagen: {
+    color: 'white',
+},
+imagen: {
     width: width * 0.4,
     height: width * 0.4,
     borderRadius: 40,
-    margin: 15
-  }
+    
+},
+gradient:{
+    width: width * 0.4,
+    height: width * 0.4,
+    borderRadius: 40
+}
 })
 export default Card;
 
