74441a3e6d057dcf52dad83936c1758d842a1817
HiUlisesOk <uesquivel95@gmail.com>
2023-01-26 13:57:53 -0300

    solicitud de adopcion creada

--- client/src/Components/Detail/SolicitudPet.jsx
+++ client/src/Components/Detail/SolicitudPet.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { ScrollView, Text, View, Image } from 'react-native'
+import { ButtonYellow } from '../Buttons/Buttons';
+
 const SolicitudPet = (item) => {
+	const { email, profilePic, message } = item.route.params;
 	return (
-		<ScrollView>
-			<Text>Solicitud de Adopción</Text>
-			<Image className="h10 w-10" source={{ uri: currentUser.profilePic }} />
+		<ScrollView className="flex">
+			<Text className="my-5 mx-auto text-3xl text-bold">Solicitud de Adopción</Text>
+			<Image style={{ width: 100, height: 100 }} className="rounded-full mx-auto my-1" source={{ uri: profilePic }} />
+			<View>
+				<Text className="my-5 mx-auto text-lg">{message}</Text>
+			</View>
+			<ButtonYellow text={"contacto"} onPress={() => console.log(email)} />
+			<ButtonYellow text={"Aceptar Solicitud"} onPress={() => alert("Solicitud Aceptada")} />
 		</ScrollView>
 	)
 }
