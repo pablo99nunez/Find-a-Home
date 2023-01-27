7ce9dc462670318ceaa2fbf8ae6b98de8ee3f72f
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-01-27 22:05:13 +0100

    fix create

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -36,6 +36,7 @@ export default function Home({ navigation }) {
     React.useCallback(() => {
       async function evitaReturnDelUseEffect() {
         dispatch(getAllPets());
+        console.log(allPets)
       }
       evitaReturnDelUseEffect(); //porq saltaba un warning, pedia autonvocarla adentro
     }, [])
