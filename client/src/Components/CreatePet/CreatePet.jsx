1090056139db35486c4de94b0122430c2e4765e3
iXGEN <106850727+iXGEN@users.noreply.github.com>
2023-01-29 19:50:12 -0300

    console log removed

--- client/src/Components/CreatePet/CreatePet.jsx
+++ client/src/Components/CreatePet/CreatePet.jsx
@@ -1,16 +1,11 @@
 import React, { useEffect } from "react";
-import {
-  StyleSheet,
-  View,
-  ScrollView,
-} from "react-native";
+import { StyleSheet, View, ScrollView } from "react-native";
 import { useState } from "react";
 import { PetPost } from "../../Redux/Actions/index";
 import * as Location from "expo-location";
 import { ButtonYellow } from "../Buttons/Buttons";
 
 export const CreatePet = ({ navigation }) => {
- 
   const [pin, setPin] = useState({
     latitude: -34.628517,
     longitude: -58.45905,
@@ -28,29 +23,25 @@ export const CreatePet = ({ navigation }) => {
         accuracy: Location.Accuracy.Highest,
         maximumAge: 10000,
       });
-      /* console.log(location); */
 
       setPin({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
       });
-      console.log("PIN EN CREATEPET", pin);
     })();
   }, []);
 
   const [crear, setCrear] = useState({
-    name: '',
-    description: '',
-    birthday: '',
-    size: '',
-    state: '',
-    specie: '',
-    galeria:[],
+    name: "",
+    description: "",
+    birthday: "",
+    size: "",
+    state: "",
+    specie: "",
+    galeria: [],
   });
 
-  const [error, setError] = useState({
-  });
-
+  const [error, setError] = useState({});
 
   const HandleSubmit = async () => {
     if (!error.name && !error.description && !error.birthday) {
@@ -59,7 +50,9 @@ export const CreatePet = ({ navigation }) => {
         description: crear.description,
         birthday: crear.birthday,
         size: crear.size,
-        profilePic: crear.galeria[0] || 'https://us.123rf.com/450wm/natbasil/natbasil1601/natbasil160100031/52068222-animales-siluetas-perro-gato-y-conejo-logotipo-de-la-tienda-de-animales-o-cl%C3%ADnica-veterinaria-ilustr.jpg?ver=6',
+        profilePic:
+          crear.galeria[0] ||
+          "https://us.123rf.com/450wm/natbasil/natbasil1601/natbasil160100031/52068222-animales-siluetas-perro-gato-y-conejo-logotipo-de-la-tienda-de-animales-o-cl%C3%ADnica-veterinaria-ilustr.jpg?ver=6",
         gallery: [...crear.galeria.slice(1)],
         specie: crear.specie,
         coordinates: {
@@ -77,13 +70,13 @@ export const CreatePet = ({ navigation }) => {
         })
         .finally((e) => {
           setCrear({
-            name: '',
-            description: '',
-            birthday: '',
-            size: '',
-            state: '',
-            specie: '',
-            galeria:[],
+            name: "",
+            description: "",
+            birthday: "",
+            size: "",
+            state: "",
+            specie: "",
+            galeria: [],
           });
         });
     } else {
@@ -93,13 +86,17 @@ export const CreatePet = ({ navigation }) => {
 
   return (
     <>
-      <ScrollView className='bg-[#d9d9d9]' >
-        <FormPet setCrear={setCrear} crear={crear} error={error} setError={setError}/>
-        <View className='my-[10%]'>
-        <ButtonYellow onPress={()=> HandleSubmit()} text={'Publicar'}/>
+      <ScrollView className="bg-[#d9d9d9]">
+        <FormPet
+          setCrear={setCrear}
+          crear={crear}
+          error={error}
+          setError={setError}
+        />
+        <View className="my-[10%]">
+          <ButtonYellow onPress={() => HandleSubmit()} text={"Publicar"} />
         </View>
       </ScrollView>
-    
     </>
   );
 };
@@ -107,6 +104,6 @@ export const CreatePet = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     paddingTop: 23,
-    backgroundColor: "#d9d9d9"
+    backgroundColor: "#d9d9d9",
   },
 });
