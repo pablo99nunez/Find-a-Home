beb554adc2e4de7fedcb6dd51beccc309c93d461
flopitt90 <flopitta_90@hotmail.com>
2023-01-31 16:50:09 +0000

    edit profile added header and back arrow

--- client/src/Components/EditProfile/EditProfile.jsx
+++ client/src/Components/EditProfile/EditProfile.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Photos } from "./Photo";
 import { useState } from "react";
 import { View, Text, TouchableOpacity, Image, TextInput} from 'react-native'
 import { ScrollView } from "react-native-gesture-handler";
 import { ButtonYellow } from "../Buttons/Buttons";
 import { EditProfiles } from "../../Redux/Actions";
-import { useDispatch } from "react-redux";
 
  const EditProfile = (props) =>{
 
@@ -37,7 +35,7 @@ const HandleSubmit = async () => {
           console.log(error.message);
         })
         .finally((e) => {
-          setCrear({
+          setProfile({
             firstName: "",
             lastName: "",
             phone: "",
@@ -49,57 +47,54 @@ const HandleSubmit = async () => {
     
 
 return(
-    <ScrollView>
-<View>
+    <ScrollView className='bg-[#d9d9d9]'>
+
+<View className='w-10/12 mx-auto'>
 <Photos profile={profile} setProfile={setProfile} name = {profile.name}/>
-</View>
+
 <View>
-<Text className='text-2xl font-extralight mb-3'>Nombre</Text>
+<Text className='text-2xl font-extralight m-2'>Nombre</Text>
 
 <TextInput
             className='h-11 bg-[#717171] rounded-md px-3 font-light'
             placeholder="Por favor actualiza tu nombre"
             placeholderTextColor="#fcfcfc"
-            // autoCapitalize="none"
             value={profile.firstName}
             onChangeText={(text) => setProfile({ ...profile, firstName: text })}
 
 />
 </View>
 <View>
-<Text className='text-2xl font-extralight mb-3'>Apellido</Text>
+<Text className='text-2xl font-extralight m-2'>Apellido</Text>
 
 <TextInput
             className='h-11 bg-[#717171] rounded-md px-3 font-light'
             placeholder="Por favor actualiza tu Apellido"
             placeholderTextColor="#fcfcfc"
-            // autoCapitalize="none"
             value={profile.lastName}
             onChangeText={(text) => setProfile({ ...profile, lastName: text })}
           maxLength={20}
           />
 </View>
 <View>
-<Text className='text-2xl font-extralight mb-3'>Telefono</Text>
+<Text className='text-2xl font-extralight m-2'>Telefono</Text>
 
 <TextInput
             className='h-11 bg-[#717171] rounded-md px-3 font-light'
             placeholder="Por favor actualiza tu telefono"
             placeholderTextColor="#fcfcfc"
-            // autoCapitalize="none"
             value={profile.phone}
             onChangeText={(text) => setProfile({ ...profile, phone: text })}
             maxLength={20}
           />
 </View>
 <View>
-<Text className='text-2xl font-extralight mb-3'>Direccion</Text>
+<Text className='text-2xl font-extralight m-2'>Direccion</Text>
 
 <TextInput
             className='h-11 bg-[#717171] rounded-md px-3 font-light'
             placeholder="Por favor actualiza tu direccion"
             placeholderTextColor="#fcfcfc"
-            // autoCapitalize="none"
             value={profile.address}
             onChangeText={(text) => setProfile({ ...profile, address: text })}
             maxLength={20}
@@ -108,6 +103,7 @@ return(
 <View className="my-[10%]">
 <ButtonYellow onPress={() => HandleSubmit()} text={"Editar"} />
 
+        </View>
         </View>
 </ScrollView>
 )
