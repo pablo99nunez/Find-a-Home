a8c376f31c0807af4ce1263435101d8b8aef2509
ebootdpr <rod.toobe@gmail.com>
2023-02-01 14:41:08 -0300

    login flow improved

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,39 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { View, StyleSheet } from "react-native";
 import { LandingButton } from "../Buttons/Buttons";
-import { useFocusEffect } from "@react-navigation/native";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../firebase/authentication";
-import { useDispatch, useSelector } from "react-redux";
-import { setIsLoggedIn } from "../../Redux/Actions";
 
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useSelector } from "react-redux";
 
 const LandingPage = ({ navigation }) => {
   const isLoggedIn = useSelector(store => store.isLoggedIn)
-  const dispatch = useDispatch();
-
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      user.getIdToken().then(async (tkn) => {
-        await AsyncStorage.setItem(
-          "authorization",
-          "Bearer " + tkn
-        )
-        dispatch(setIsLoggedIn(true));
-        //No mover este console log de acá
-        console.log("authorization", "Bearer " + tkn);
-      }).catch((err) => {
-        console.error("⚠️ Error -> 🚨 LANDING PAGE -> 🔔onAuthStateChanged: " + err.message);
-      })
-    } else {
-      AsyncStorage.clear();
-      dispatch(setIsLoggedIn(false));
-    }
-  });
-
-
-
+  
   return (
     <View style={styles.container}>
       {isLoggedIn ? (
