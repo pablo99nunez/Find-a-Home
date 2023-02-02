6643ae7da39891ea6af9b1ae4caeccb291de8ef9
HiUlisesOk <uesquivel95@gmail.com>
2023-02-02 02:51:10 -0300

    fix: push notification

--- api/src/routes/pushNotify.route.js
+++ api/src/routes/pushNotify.route.js
@@ -9,20 +9,20 @@ router.post("/push-notify", async (req, res) => {
 
 	try {
 		const response = await axios.post("https://exp.host/--/api/v2/push/send", {
-			to: `ExponentPushToken[${token}]`,
+			to: `${token}`,
 			title,
 			body
 		}, {
 			headers: {
-				host: 'exp.host',
-				accept: 'application/json',
+				'host': 'exp.host',
+				'accept': 'application/json',
 				'accept-encoding': 'gzip, deflate',
 				'content-type': 'application/json'
 			}
 		});
 
 		console.log(response.data);
-		res.status(200).json({ message: "Push notification sent" });
+		res.status(200).json({ message: "Push notification sent", response: response.data });
 	} catch (error) {
 		console.error("⚠️ Error -> 🚨 Routes -> 🔔/push-notify: " + error.message);
 		res.status(500).json({ error: "Internal server error" });
