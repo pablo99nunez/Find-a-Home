5bc2e58509658fb728e3cc571ca0fac253938b5b
flopitt90 <flopitta_90@hotmail.com>
2023-02-07 01:35:02 +0000

    android build

--- client/metro.config.js
+++ client/metro.config.js
@@ -1,16 +1,4 @@
-const { getDefaultConfig } = require("metro-config");
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts }
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
-  };
-})();
+const { getDefaultConfig } = require('@expo/metro-config')
+const defaultConfig = getDefaultConfig(__dirname)
+defaultConfig.resolver.sourceExts.push('cjs')
+module.exports = defaultConfig
