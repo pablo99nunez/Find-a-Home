a61f089ecd306540dcc861d459e6d4d8dcb25a34
Pablo Nunez <pablo.nunez01@globant.com>
2023-02-23 13:54:45 -0300

    Fix require

--- api/api/index.js
+++ api/api/index.js
@@ -1,12 +1,14 @@
-const server = require('./server'); //se mete en el contexto de ejecucion de server.js
+const server = require('../src/server'); //se mete en el contexto de ejecucion de server.js
 //luego de que termine de ejecutarse, sigue a la siguiente linea
 //crea una constante port
 
 const port = process.env.PORT || 8080;
 //ejecuta el metodo .create() que pertenese a express (server.js exporta app)
-server.create()
-    .then(app => {
-        app.listen(port, () => {
-            console.log(`Server has started on port ${port}!`);
-        });
-    }).catch(err => console.log(err));
+server
+  .create()
+  .then((app) => {
+    app.listen(port, () => {
+      console.log(`Server has started on port ${port}!`);
+    });
+  })
+  .catch((err) => console.log(err));
