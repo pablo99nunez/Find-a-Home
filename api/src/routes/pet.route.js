646d4c0f3573b7f514a0a8c4d4fb4db27b5c22b1
pablo99nunez <pablo99nunez@gmail.com>
2023-02-25 18:49:00 -0300

    Fix error code pet post route

--- api/src/routes/pet.route.js
+++ api/src/routes/pet.route.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const { createPet } = require('../controllers/createPet');
-const { findPet, findAllPets, updatePet, filterByOwner,denPet } = require('../controllers/petController');
-const {solicitarAdopcion} = require('../controllers/adoptionController')
+const {
+  findPet,
+  findAllPets,
+  updatePet,
+  filterByOwner,
+  denPet,
+} = require('../controllers/petController');
+const { solicitarAdopcion } = require('../controllers/adoptionController');
 const { checkJwt } = require('../utils/firebase-stuff');
 const { deletePet } = require('../controllers/deletePet');
 const { limit5cada12horas } = require('../utils/rate-limiters');
@@ -10,122 +16,127 @@ const router = express.Router();
 //todos //
 router.get('/', async (req, res) => {
   try {
-    const allPets = await findAllPets({})
-    res.send({message: 'Todas las mascotas', payload: allPets})
+    const allPets = await findAllPets({});
+    res.send({ message: 'Todas las mascotas', payload: allPets });
   } catch (error) {
-    res.status(501).send( error.message)
+    res.status(501).send(error.message);
   }
-})
+});
 
-router.get('/poride/:id', async (req, res) => { 
-  const petId = req.params.id
+router.get('/poride/:id', async (req, res) => {
+  const petId = req.params.id;
   try {
-    const getPetById = await findPetById(petId)
-    res.send({message: 'Todas las mascotas', payload: getPetById})
+    const getPetById = await findPetById(petId);
+    res.send({ message: 'Todas las mascotas', payload: getPetById });
   } catch (error) {
-    res.status(501).send({ error: error.message })
+    res.status(501).send({ error: error.message });
   }
-})
+});
 
 //CREAR PET
 //logeado, user, checkJwt te deja pasar si el token esta bien, y ademas
 //mete adentro del req una propiedad user con los datos del usuario
-router.post('/',checkJwt, async (req, res) => {
+router.post('/', checkJwt, async (req, res) => {
   try {
-    const PetData = req.body
-    const newPet = await createPet(PetData, req.user.email)
-    res.status(200).send({ message: 'Mascota creada', payload: newPet })
+    const PetData = req.body;
+    const newPet = await createPet(PetData, req.user.email);
+    res.status(200).send({ message: 'Mascota creada', payload: newPet });
   } catch (error) {
-    res.status(501).send( error.message)
-
-
+    res.status(400).send(error);
   }
-})
+});
 
 //Pet por id en body pasar id
 router.get('/profile/', async (req, res) => {
   try {
-  
-    const pet = await findPet(req.body.id)
-    res.send({ message: 'Mascota encontrada', payload: pet})
-  
+    const pet = await findPet(req.body.id);
+    res.send({ message: 'Mascota encontrada', payload: pet });
   } catch (error) {
-    res.status(501).send( error.message)
-  }
-})
-
-//logged, user, modifica el PET con lo q le pase por body
-router.put('/profile', checkJwt, async (req, res) => {
-  try {//prohibir modificar el owner y la history
-    const newData = req.body
-    const updatedPet = await updatePet(newData, req.body.id, req.user.email)
-    res.send({ message: 'Mascota modificada', payload: updatedPet });
-  } catch (err) {
-    res.status(501).send( err.message)
+    res.status(501).send(error.message);
   }
 });
 
+//logged, user, modifica el PET con lo q le pase por body
+router.put('/profile', checkJwt, async (req, res) => {
+  try {
+    //prohibir modificar el owner y la history
+    const newData = req.body;
+    const updatedPet = await updatePet(newData, req.body.id, req.user.email);
+    res.send({ message: 'Mascota modificada', payload: updatedPet });
+  } catch (err) {
+    res.status(501).send(err.message);
+  }
+});
 
 //solicitar perro
 router.put('/profile/solicitud', checkJwt, async (req, res) => {
   try {
     //solicitarAdopcion = async (petID, message, interestedEmail, deleteSolicitud)
-    const message = req.body.message
-    const petID = req.body.petID
-    const petConNuevaSolicitud = await solicitarAdopcion(petID, message, req.user.email)
+    const message = req.body.message;
+    const petID = req.body.petID;
+    const petConNuevaSolicitud = await solicitarAdopcion(
+      petID,
+      message,
+      req.user.email
+    );
     res.send({ message: 'Solicitud Enviada', payload: petConNuevaSolicitud });
   } catch (err) {
-    res.status(501).send( err.message)
+    res.status(501).send(err.message);
   }
 });
 
 //quitar solicitud, pasar petID por body
-router.delete('/profile/solicitud',checkJwt, async (req, res) => {
+router.delete('/profile/solicitud', checkJwt, async (req, res) => {
   try {
     //solicitarAdopcion = async (petID, message, interestedEmail, deleteSolicitud)
-    const petID = req.body.petID
-    const message = req.body.message
-    const petSinTuSolicitud = await solicitarAdopcion(petID, message, req.user.email,true)
+    const petID = req.body.petID;
+    const message = req.body.message;
+    const petSinTuSolicitud = await solicitarAdopcion(
+      petID,
+      message,
+      req.user.email,
+      true
+    );
     res.send({ message: 'Solicitud Eliminada', payload: petSinTuSolicitud });
   } catch (err) {
-    res.status(501).send( err.message)
+    res.status(501).send(err.message);
   }
 });
-
 
 //SOLO ADMIN PUEDE BORRAR PERROS
-router.delete('/profile/:ID',checkJwt, async (req, res) => {
+router.delete('/profile/:ID', checkJwt, async (req, res) => {
   try {
-    const deletedPet = await deletePet(req.params.ID)
+    const deletedPet = await deletePet(req.params.ID);
     res.send({ message: 'Mascota borrada', payload: deletedPet });
   } catch (err) {
-    res.status(501).send( err.message)
+    res.status(501).send(err.message);
   }
 });
 
-router.get('/byowner',checkJwt, async (req, res) => {
+router.get('/byowner', checkJwt, async (req, res) => {
   try {
-    if(req.query.email){
-      const pets = await filterByOwner(req.query.email)
+    if (req.query.email) {
+      const pets = await filterByOwner(req.query.email);
       res.status(200).send(pets);
-    }else{
-   const email = req.user.email
+    } else {
+      const email = req.user.email;
 
-    const allPets = await filterByOwner(email);
+      const allPets = await filterByOwner(email);
 
-    res.send(allPets);
-  }
+      res.send(allPets);
+    }
   } catch (error) {
-    res.status(501).send( error.message);
+    res.status(501).send(error.message);
   }
 });
 router.put('/denunciar', checkJwt, async (req, res) => {
-  try {//prohibir modificar el owner y la history
-    const denuncia = req.body
-    const updatedPet = await denPet(denuncia, req.body.id, req.user.email)
+  try {
+    //prohibir modificar el owner y la history
+    const denuncia = req.body;
+    const updatedPet = await denPet(denuncia, req.body.id, req.user.email);
     res.send({ message: 'mascota denunciada', payload: updatedPet });
   } catch (err) {
-    res.status(501).send( err.message)
+    res.status(501).send(err.message);
   }
 });
 module.exports = router;
